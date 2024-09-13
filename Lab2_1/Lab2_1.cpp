3223ad2578e4c7527d5b51acc6fc55ba0bf60dd5
Tylko-jedno <diogkek@gmail.com>
2024-09-13 21:53:52 +0300

    first master commit

---
+++ Lab2_1/Lab2_1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double x, a, z1, z2;
+
+	cout << "x = "; cin >> x;
+	cout << "\na = "; cin >> a;
+
+	z1 = cos(a) + sin(a) + cos(3 * a) + sin(3 * a);
+	z2 = 2 * sqrt(2) * cos(a) * sin(((4 * atan(1.) / 4) + 2 * a));
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+
+}
